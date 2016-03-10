04ed3238e4bee5ed085cd7ee5065f7ed33ca0b24
李聪会 <651505992@qq.com>
2016-03-10 17:19:24 +0800

    log

--- testGitHub/ViewController.m
+++ testGitHub/ViewController.m
@@ -17,12 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
-    
-    
-    
-    
-    
+    NSLog(@"我看看看啊");
+     NSLog(@"我看看看啊");
+     NSLog(@"我看看看啊");
+     NSLog(@"我看看看啊");
+     NSLog(@"我看看看啊");
+     NSLog(@"我看看看啊");
     
     
     // Do any additional setup after loading the view, typically from a nib.
